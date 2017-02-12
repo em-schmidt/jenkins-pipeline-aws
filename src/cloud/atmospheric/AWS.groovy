c605cb151422212b3242efc5681d321b9ad31856
eric m. schmidt <eric@moven.com>
2017-02-12 14:48:10 -0500

    still messing with the grab

--- src/cloud/atmospheric/AWS.groovy
+++ src/cloud/atmospheric/AWS.groovy
@@ -1,7 +1,6 @@
 package cloud.atmospheric.AWS
 
-import groovy.grape.Grape
-Grape.grab(group:'com.amazonaws', module:'aws-java-sdk', version:'1.11.89')
+@Grab('com.amazonaws:aws-java-sdk:1.11.89')
 
 import com.amazonaws.services.ec2.AmazonEC2Client
 
