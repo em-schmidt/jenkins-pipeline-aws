d904f8a36775a850adaf2d92e464bfd5a236140c
eric m. schmidt <eric@moven.com>
2017-02-11 21:50:16 -0500

    aws regionlist

---
+++ src/cloud/atmospheric/AWS.groovy
@@ -0,0 +1,15 @@
+@Grab('com.amazonaws:aws-java-sdk:1.11.89')
+
+package cloud.atmospheric.AWS
+import com.amazonaws.services.ec2.AmazonEC2Client
+
+String regionslist {
+    ec2 = new AmazonEC2Client()
+    regions = ec2.describeRegions().getRegions()
+
+    regionlist = ""
+    regions.each {
+        regionlist += it.getRegionName() + "\n"
+    }
+    retrun regionlist
+}
