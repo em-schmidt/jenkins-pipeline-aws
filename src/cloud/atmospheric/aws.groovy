ba5ab87231164d322aa4518b36b936443b94d02e
eric m. schmidt <eric@moven.com>
2017-02-12 16:55:49 -0500

    update aws client

--- src/cloud/atmospheric/aws.groovy
+++ src/cloud/atmospheric/aws.groovy
@@ -2,10 +2,10 @@ package cloud.atmospheric
 
 @Grab('com.amazonaws:aws-java-sdk:1.11.89')
 
-import com.amazonaws.services.ec2.AmazonEC2Client
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
 
     def regionList() {
-        ec2 = new AmazonEC2Client()
+        ec2 = AmazonEC2Client.defaultClient()
         regions = ec2.describeRegions().getRegions()
 
         regionlist = ""
